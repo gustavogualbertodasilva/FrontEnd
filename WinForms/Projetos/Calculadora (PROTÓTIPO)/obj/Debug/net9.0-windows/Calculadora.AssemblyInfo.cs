a0b2fdb272c31bc027dffdcf77a51ffeafbbd6b2
SeuNomeDeUsuario <gustavogualbertodasilva04@gmail.com>
2025-11-29 12:03:51 -0300

    +1 WallPaper

--- WinForms/Projetos/Calculadora (PROTÓTIPO)/obj/Debug/net9.0-windows/Calculadora.AssemblyInfo.cs
+++ WinForms/Projetos/Calculadora (PROTÓTIPO)/obj/Debug/net9.0-windows/Calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42f0c7cc7182889c94dd2f3fdce4b5934d524908")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc0f73fe5215e7be2db6afabebc20cf8c3f006c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
